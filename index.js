01b0ad9aaf2653c988aabe4c4eb45142bb81c414
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-21 13:58:56 +0600

    added a delete route at /toys/:id for deleting a specific toy based on id

--- index.js
+++ index.js
@@ -142,6 +142,17 @@ async function run() {
         });
 
 
+        // delete route for deleting/removing a toy
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
