ab8309122f6f59b374085c3447b248d6fb205df0
swapnilaanam <swapnilaanam211@gmail.com>
2023-10-21 12:10:26 +0600

    added routes for payment and mycollection

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fuichu5.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,8 +29,11 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const userCollection = client.db('toysDB').collection('users');
         const subCategoryCollection = client.db('toysDB').collection('subcategories');
         const toyCollection = client.db('toysDB').collection('toys');
+        const myCollection = client.db("toysDB").collection("mycollections")
+        const paymentCollection = client.db('toysDB').collection('payments');
 
         // const indexKeys = { toyName: 1 };
 
@@ -36,12 +41,44 @@ async function run() {
 
         // const result = await toyCollection.createIndex(indexKeys, indexOptions);
 
+        // users route
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const result = await userCollection.findOne({ email: email });
+            // console.log(result);
+            res.send(JSON.stringify(result));
+        });
+
+        app.get('/users/role/owner/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const user = await userCollection.findOne({ email: email });
+
+            if (user?.role === 'Owner') {
+                res.send(true);
+            }
+            else {
+                res.send(false);
+            }
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+
+            // console.log(newUser);
+
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        });
+
+
         // get route for all sub categories
 
         app.get('/subcategories', async (req, res) => {
             const cursor = subCategoryCollection.find();
 
-            const result = await cursor.toArray();
+            const result = await cursor.sort({ _id: 1 }).toArray();
             res.send(result);
         });
 
@@ -99,7 +136,6 @@ async function run() {
             const result = await toyCollection.findOne(query);
             res.send(result);
         });
-
 
         // get route for searching by name in all toys
         app.get('/toys/search/toyname', async (req, res) => {
@@ -164,6 +200,26 @@ async function run() {
         });
 
 
+        app.patch('/toys/quantity/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const filter = { _id: new ObjectId(id) };
+
+            const toy = await toyCollection.findOne(filter);
+
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    quantity: toy?.quantity - 1
+                }
+            };
+
+            const result = await toyCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+
         // delete route for deleting/removing a toy
         app.delete('/toys/:id', async (req, res) => {
             const id = req.params.id;
@@ -174,6 +230,96 @@ async function run() {
             res.send(result);
         });
 
+
+
+        // my collection route
+        app.get('/mycollections', async (req, res) => {
+            const {email} = req.query;
+            
+            const query = {buyerEmail: email};
+
+            const result = await myCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/mycollections/sort', async (req, res) => {
+            const {order, email} = req.query;
+
+            const query = {buyerEmail: email};
+
+            if(order === "Ascending") {
+                const cursor = myCollection.find(query).sort({price: 1}).collation({locale: "en_US", numericOrdering: true});
+
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+
+            if(order === "Descending") {
+                const cursor = myCollection.find(query).sort({price: -1}).collation({locale: "en_US", numericOrdering: true});
+
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+
+        })
+
+
+        app.post('/mycollections', async (req, res) => {
+            const newToy = req.body;
+
+            const toy = await myCollection.findOne({ toyId: newToy?.toyId });
+
+            if (toy) {
+                const filter = { toyId: newToy?.toyId };
+
+                const options = { upsert: true };
+
+                const updateDoc = {
+                    $set: {
+                        quantity: toy?.quantity + 1
+                    }
+                };
+
+                const result = await myCollection.updateOne(filter, updateDoc, options);
+                res.send(result);
+            }
+            else {
+                const result = await myCollection.insertOne(newToy);
+                res.send(result);
+            }
+        })
+
+
+
+        // Payment Intent
+        app.post('/create-payment-intent', async (req, res) => {
+            let { price } = req.body;
+
+            price *= 100;
+            price = Number(price.toFixed(2));
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: price,
+                currency: "usd",
+                payment_method_types: [
+                    "card"
+                ],
+            });
+
+            // console.log(paymentIntent);
+
+            res.send({
+                clientSecret: paymentIntent?.client_secret,
+            })
+        })
+
+        // payment route
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+
+            const result = await paymentCollection.insertOne(payment);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
